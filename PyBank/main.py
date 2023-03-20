63ef2506061a2210b23370900fe967c6d690c29b
relseykichards <kelsrichards28@gmail.com>
2023-03-20 18:27:20 -0500

    fixed month count

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,12 +7,12 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     # print(csvreader)
     csv_header = next(csvreader)
-    # print(f"CSV Header: {csv_header}")
 # MONTH COUNT
+    month_count = sum(1 for row in csvreader)
+    print(month_count)
+
     for row in csvreader: 
-        data = list(csvreader)
-        row_count = len(data)
-        print(row_count)
+       print(row)
 
 def  net_total_sum():
     net_total= 0
