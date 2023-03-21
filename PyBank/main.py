497c2eabe5361bb21682edb5a9d9d6748810f9c1
relseykichards <kelsrichards28@gmail.com>
2023-03-21 16:56:19 -0500

    completed month count and profit/loss for the period

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,21 +7,13 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     # print(csvreader)
     csv_header = next(csvreader)
-# MONTH COUNT
-    month_count = sum(1 for row in csvreader)
-    print(month_count)
 
-    for row in csvreader: 
-       print(row)
-
-def  net_total_sum():
-    net_total= 0
-    # reader=csv.reader
-    # for line in reader:
-    net_total+=int(row[1])
-    return net_total
-        
-print(net_total_sum())
-
+    val=0
+    count=0
+    for line in csvreader:
+        val=val+int(line[1])
+        count=count+1
+    print(f"The total number of months in this period are: {val}")
+    print(f"The total profit/losses during this period is: {count}")
 
 
